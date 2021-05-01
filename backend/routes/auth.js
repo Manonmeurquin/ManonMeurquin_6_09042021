6bc8a763b3486363f25e8143b0a92ee01f711230
Manon Meurquin <manon.meurquin@free.fr>
2021-05-01 21:33:33 +0200

    middleware rate-limit

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authCtrl = require('../controllers/auth');
+const rateLimit = require('../middleware/rate-limit');
 
 router.post('/signup', authCtrl.signup);
-router.post('/login', authCtrl.login);
+router.post('/login', rateLimit, authCtrl.login);
 
 module.exports = router;
